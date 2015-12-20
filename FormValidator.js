62675afe2942c621e940d8cecde9fa5007492283
hiddendev <coquelet.c@gmail.com>
2015-12-20 15:59:48 +0100

    remove dead code

--- FormValidator.js
+++ FormValidator.js
@@ -1,14 +1,3 @@
-// (function () {
-//     var forms = document.querySelectorAll('form');
-//     console.log(forms);
-//     for (var i = 0; i < forms.length; i++) {
-//         console.log(forms[i].attributes);
-//         for (var j = 0; j < forms[i].attributes.length; j++) {
-//             console.log(forms[i].attributes[j]);
-//         }
-//     }
-// })(this);
-
 (function () {
     var stop = function (e, msg) {
         alert(msg);
